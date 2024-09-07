05679b9a021c1f74e95560104984dd8ffbc55e3d
ibrahimcahit <i.zdemir2001@gmail.com>
2024-09-07 20:36:00 +0300

    v0.1.0
    /***********************************************************
    *                 INS
    *
    * Firmware Version :  FW_H7_V1.10.0
    * IDE Version      :  v1.10.1
    * MCU              :  STM32H743VIT6
    *
    * Version :  v0.1.0
    * Creator :  ibrahimcahit
    * Date    :  2024/08/07
    *
    * Changelog:
    * --------------------------------------------------------
    * - Initial Commit
    *
    ***********************************************************/

---
+++ Core/versiyon.h
@@ -0,0 +1,35 @@
+/*
+ * versiyon.h
+ *
+ *  Created on: Sep 7, 2024
+ *      Author: ibrahimcahit
+ */
+
+#ifndef VERSIYON_H_
+#define VERSIYON_H_
+
+#define VERSION_MAJOR    0
+#define VERSION_MINOR    1
+#define VERSION_BUILD    0
+
+#define VERSION          "0_1_0"
+
+/***********************************************************
+*                 INS
+*
+* Firmware Version :  FW_H7_V1.10.0
+* IDE Version      :  v1.10.1
+* MCU              :  STM32H743VIT6
+*
+* Version :  v0.1.0
+* Creator :  ibrahimcahit
+* Date    :  2024/08/07
+*
+* Changelog:
+* --------------------------------------------------------
+* - Initial Commit
+*
+***********************************************************/
+
+
+#endif /* VERSIYON_H_ */
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # INS
-INS
+
+Refined version of my previous projects, which are based on:
+
+* STM32H7
+* BMI160 IMU
+* Kalman Filter and Estimator Library
